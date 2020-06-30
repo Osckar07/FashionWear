ce067f1a9f078aaeab6f5ccac5bf45395f1665e4
Abel <enriquezun3@gmail.com>
2020-06-29 19:13:56 -0600

    Creada la ruta para registrarse, aun sin vista. Agregado un icono para la pestaña del navegador,

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const routes = express.Router();
 
 // Importar los controladores
 const productosController = require("../controllers/productosController");
+const usuariosController = require("../controllers/usuariosController");
 
 // Construir las rutas disponibles para el servidor
 module.exports = function () {
   
   routes.get("/", productosController.inicioProductos);
+
+  routes.get("/crear_cuenta", usuariosController.formularioRegistrarse);
 
   return routes;
 };
